4a9554778d99c8eb66eae60a2404242c6a98d845
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-23 16:54:06 +0530

    updated

--- src/test/java/com/mindtree/ArithmeticOperations/ArithmeticOperationsTest.java
+++ src/test/java/com/mindtree/ArithmeticOperations/ArithmeticOperationsTest.java
@@ -1,8 +1,8 @@
 package com.mindtree.ArithmeticOperations;
 
-	import com.mindtree.ArithmeticOperations.App;
+import com.mindtree.ArithmeticOperations.App;
 	
-	public class ArithmeticOperationsTest {
+/*		public class ArithmeticOperationsTest {
 	private App objCalcUnderTest;
 
 	
@@ -51,4 +51,62 @@ package com.mindtree.ArithmeticOperations;
 	objCalcUnderTest.divide(a, b);
 	} 
 	}
+	*/
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Rishiraj
+ */
+public class ArithmeticOperationsTest {
+    
+    private App cal;
+
+    @Before
+    public void setUp() throws Exception {
+        this.cal= new calculator();
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        cal = null;
+    }
+
+    @Test
+    public void Testadd() throws Exception {
+        assertEquals(8,cal.add(5,3));
+
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        assertEquals(10,cal.sub(18,8));
+    }
+
+    @Test
+    public void testMultiplyByZero() throws Exception {
+        assertEquals(0,cal.mul(2,0));
+    }
+
+    @Test
+    public void testDivideByZero() throws Exception {
+        assertEquals(0,cal.dividebyZero(2,0));
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        assertEquals(15,cal.mul(3,5));
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        assertEquals(5,cal.testdevide(10,2));
+    }
+}
 
