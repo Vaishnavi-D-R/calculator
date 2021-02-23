2536c1c9ce3f5de9bed6ad4d05ee167e7c5ec7df
Vaishnavi-D-R <78353427+Vaishnavi-D-R@users.noreply.github.com>
2021-02-23 17:00:57 +0530

    updated

--- src/test/java/com/mindtree/ArithmeticOperations/ArithmeticOperationsTest.java
+++ src/test/java/com/mindtree/ArithmeticOperations/ArithmeticOperationsTest.java
@@ -1,17 +1,20 @@
 package com.mindtree.ArithmeticOperations;
 
-import com.mindtree.ArithmeticOperations.App;
-	
-/*		public class ArithmeticOperationsTest {
+	import com.mindtree.ArithmeticOperations.App;
+	import org.junit.Test; 
+	import org.junit.Assert; 
+	import org.junit.Before;
+
+	public class ArithmeticOperationsTest {
 	private App objCalcUnderTest;
 
-	
+	@Before
 	public void setUp() {
 	//Arrange
 	objCalcUnderTest = new App();
 	}
 
-	
+	@Test
 	public void testAdd() { 
 	int a = 15; int b = 20; 
 	int expectedResult = 35;
@@ -21,7 +24,7 @@ import com.mindtree.ArithmeticOperations.App;
 	Assert.assertEquals(expectedResult, result);
 	}
 
-	
+	@Test
 	public void testSubtract() {
 	int a = 25; int b = 20; 
 	int expectedResult = 5; 
@@ -29,7 +32,7 @@ import com.mindtree.ArithmeticOperations.App;
 	Assert.assertEquals(expectedResult, result);
 	}
 
-	
+	@Test
 	public void testMultiply() {
 	int a = 10; int b = 25;
 	long expectedResult = 250;
@@ -37,7 +40,7 @@ import com.mindtree.ArithmeticOperations.App;
 	Assert.assertEquals(expectedResult, result);
 	}
 
-	
+	@Test
 	public void testDivide() {
 	int a = 56; int b = 10; 
 	double expectedResult = 5.6; 
@@ -45,68 +48,9 @@ import com.mindtree.ArithmeticOperations.App;
 	Assert.assertEquals(expectedResult, result,0.00005); 
 	}
 
-	
+	@Test(expected = IllegalArgumentException.class)
 	public void testDivideByZero() { 
 	int a = 15; int b = 0;
 	objCalcUnderTest.divide(a, b);
 	} 
 	}
-	*/
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-/**
- *
- * @author Rishiraj
- */
-public class ArithmeticOperationsTest {
-    
-    private App cal;
-
-    @Before
-    public void setUp() throws Exception {
-        this.cal= new calculator();
-        
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        cal = null;
-    }
-
-    @Test
-    public void Testadd() throws Exception {
-        assertEquals(8,cal.add(5,3));
-
-    }
-
-    @Test
-    public void testSubtract() throws Exception {
-        assertEquals(10,cal.sub(18,8));
-    }
-
-    @Test
-    public void testMultiplyByZero() throws Exception {
-        assertEquals(0,cal.mul(2,0));
-    }
-
-    @Test
-    public void testDivideByZero() throws Exception {
-        assertEquals(0,cal.dividebyZero(2,0));
-    }
-
-    @Test
-    public void testMultiply() throws Exception {
-        assertEquals(15,cal.mul(3,5));
-    }
-
-    @Test
-    public void testDivide() throws Exception {
-        assertEquals(5,cal.testdevide(10,2));
-    }
-}
-
